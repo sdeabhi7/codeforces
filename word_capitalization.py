c9324af360a6fae995bf07cacf0531a87ccf4f34
sdeabhi7 <sdeabhi@gmail.com>
2026-03-07 22:30:21 +0530

    Word Capitalization

---
+++ word_capitalization.py
@@ -0,0 +1,28 @@
+'''
+Capitalization is writing a word with its first letter as a capital letter. Your task is to capitalize the given word.
+
+Note, that during capitalization all the letters except the first one remains unchanged.
+
+Input
+A single line contains a non-empty word. This word consists of lowercase and uppercase English letters. The length of the word will not exceed 103.
+
+Output
+Output the given word after capitalization.
+
+Examples
+
+Input
+ApPLe
+
+Output
+ApPLe
+
+Input
+konjac
+
+Output
+Konjac
+'''
+
+n = input()
+print(n[0].upper() + n[1:])

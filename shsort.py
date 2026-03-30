1b6ede86f794c5d7857e9c552f9468a515d032db
sdeabhi <sdeabhi@gmail.com>
2026-03-30 17:13:19 +0000

    Sort

---
+++ shsort.py
@@ -0,0 +1,9 @@
+n = int(input())
+for i in range(n):
+    k = input()
+    for i, j in enumerate(k):
+        if (i == 0 and j == 'a')  or (i == 1 and j == 'b') or (i == 2 and j == 'c'):
+            print('YES')
+            break
+    else:
+        print('NO')

ae0caf9d9ae0a5546e15a37fa4c50e6433dc72fa
sdeabhi7 <sdeabhi@gmail.com>
2026-05-15 23:33:56 +0530

    Maximum in Table

---
+++ maxintable.py
@@ -0,0 +1,13 @@
+k = int(input())
+if k > 2:
+    f_row = [1] * k
+    value = [f_row]
+    m, n = k, k
+    for i in range(1, m):
+        t_row = [1]
+        for j in range(1, n):
+            t_row.append(t_row[-1] + value[i-1][j])
+        value.append(t_row)
+    print(value[-1][-1])
+else:
+    print(k)

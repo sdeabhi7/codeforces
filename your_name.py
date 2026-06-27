309731641c58bad1c70fecdeccdb5e2c37661510
sdeabhi7 <sdeabhi@gmail.com>
2026-06-27 16:35:46 +0530

    your name

---
+++ your_name.py
@@ -0,0 +1,8 @@
+n = int(input())
+for i in range(n):
+    k = int(input())
+    name = list(map(str, input().split()))
+    if sorted(name[0]) == sorted(name[1]):
+        print('Yes')
+    else:
+        print('No')
